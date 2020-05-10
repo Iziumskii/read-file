5eabb54462c3d7ecf1835236a50eda27b57dee47
Kirill Iizumskii <mr.llirus@gmail.com>
2020-05-10 16:55:22 +0300

    fix cookbook

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def add_new_dish(feed, book):
     book.update({feed: []})
 
 
-def add_new_recipe(feed, book, new_ingredient_name, new_quantity, new_measure):
+def add_recipe(feed, book, new_ingredient_name, new_quantity, new_measure):
     book[feed].append({"ingredient_name": new_ingredient_name, "quantity": new_quantity, "measure": new_measure})
 
 
@@ -17,7 +17,36 @@ def add_ingredient(new_ingredient, book):
             book.update({new_ingredient: {}})
 
 
-def get_shop_list_by_dishes(dishes, book):
+def making_recipe(data):
+    recipe = data.readline().strip().split()
+    return recipe[-1], recipe[-3], ' '.join(recipe[:-4])
+
+
+def create_recipe(amount, dish, book):
+    for i in range(amount):
+        measure, quantity, ingredient_name = making_recipe(f)
+        add_recipe(dish, book, ingredient_name, quantity, measure)
+
+
+def make_cookbook(data, book):
+    while True:
+        dish = data.readline().strip()
+        if not dish:
+            break
+        add_new_dish(dish, book)
+        amount_dish = int(data.readline().rstrip())
+        create_recipe(amount_dish, dish, book)
+        data.readline()
+
+
+def create_cookbook(data):
+    cook_book = {}
+    make_cookbook(data, cook_book)
+    pprint(cook_book)
+    return cook_book
+
+
+def get_shop_list_by_dishes(book, dishes, person_count):
     list_by_dishes = {}
     number = 0
     for dish_in_book in dishes:
@@ -25,33 +54,16 @@ def get_shop_list_by_dishes(dishes, book):
             for ingredient in book.get(dishes[number]):
                 if ingredient['ingredient_name'] in list_by_dishes.keys():
                     ing_name = list_by_dishes.get(ingredient['ingredient_name'])
-                    upd_quantity = int(ing_name.get('quantity')) + int(ingredient['quantity'])
+                    upd_quantity = int(ing_name.get('quantity')) + int(ingredient['quantity']) * person_count
                     list_by_dishes.update({ingredient['ingredient_name']: {"quantity": upd_quantity,
                                                                            "measure": ingredient['measure']}})
                 else:
-                    list_by_dishes.update({ingredient['ingredient_name']: {"quantity": ingredient['quantity'],
+                    list_by_dishes.update({ingredient['ingredient_name']: {"quantity": int(ingredient['quantity']) *
+                                                                                       person_count,
                                                                            "measure": ingredient['measure']}})
         number += 1
     pprint(list_by_dishes)
 
 
-cook_book = {}
-
 with open("cookbook.txt", encoding='utf8') as f:
-    while True:
-        dish = f.readline().strip()
-        if not dish:
-            break
-        add_new_dish(dish, cook_book)
-        amount = int(f.readline().rstrip())
-        for i in range(amount):
-            recipe = f.readline().strip().split()
-            measure = recipe[-1]
-            quantity = recipe[-3]
-            ingredient = recipe[:-4]
-            ingredient_name = ' '.join(ingredient)
-            add_new_recipe(dish, cook_book, ingredient_name, quantity, measure)
-        f.readline()
-    pprint(cook_book)
-
-    get_shop_list_by_dishes(['Фахитос', 'Омлет'], cook_book)
+    get_shop_list_by_dishes(create_cookbook(f), ['Фахитос', 'Омлет'], 2)
